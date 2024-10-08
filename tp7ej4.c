0c2c5f25705a11df5b1487708f17f6bf3ba90514
Fran <143757152+K7devl@users.noreply.github.com>
2024-10-08 12:48:15 -0300

    tp7ej4.c
    ;) mi primer pila

---
+++ tp7ej4.c
@@ -0,0 +1,30 @@
+#include "tp7ej4.h"
+
+void ingre(Pila *pila, int *cant){
+    int i, aux, aux1;
+    printf("Ingrese la cantidad de numeros que desea ingresar: ");
+    scanf(" %d", &aux1);
+    *cant = *cant + aux1;
+        for(i=0;i<aux1;i++){
+            printf("Ingrese el numero <%d> ", i);
+            scanf(" %d", &aux);
+            push(pila, aux);
+        }
+}
+
+void muestra(Pila *pila, int cant){
+    int i;
+    for(i=cant;i>0;i--){
+        printf("Numero <%d> ----> %d\n", i, copyy(pila));
+        suppress(pila);
+    }
+}
+
+int main(){
+    Pila pila;
+    int cant, i;
+    cant = 0;
+    init(&pila);
+    ingre(&pila, &cant);
+    muestra(&pila, cant);
+}
